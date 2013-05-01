18280b22a7b50f45eab0a79ffbd1dba627952786
Frank Bennett <biercenator@gmail.com>
2013-05-02 07:44:13 +0900

    Back to status quo ante

--- plugin/release.sh
+++ plugin/release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#set -e
+set -e
 
 # Go through the release steps for rtf-odf-scan-for-zotero
 
@@ -152,9 +152,7 @@ mv rtf-odf-scan-for-zotero.zip ..
 rm -f rtf-odf-scan-for-zotero.*
 rm -f install.bak  update.bak  updateInfo.xhtml  update.rdf
 rm -f rtf-odf-scan-for-zotero.zip
-#set +e
 git commit -m "Pre-release update" -a
-#set -e
 cd ..
 git push
 git checkout gh-pages
