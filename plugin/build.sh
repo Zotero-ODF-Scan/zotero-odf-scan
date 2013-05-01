8a052df2849cefa29bdfa201383a75d8572ec996
Frank Bennett <biercenator@gmail.com>
2013-05-02 07:30:42 +0900

    Allow filenames with spaces in xpi build

--- plugin/build.sh
+++ plugin/build.sh
@@ -7,4 +7,4 @@ find . -name '*.sh' -prune -o \
        -name 'rtf-odf-scan-for-zotero.*' -prune -o \
        -name '*~' -prune -o \
        -wholename './about.xul' -prune -o \
-       -print | xargs zip rtf-odf-scan-for-zotero.xpi
+       -print0 | xargs -0 zip rtf-odf-scan-for-zotero.xpi
