50ea9e45eb3f481367b02e7069aee9de2baddbf6
Frank Bennett <biercenator@gmail.com>
2013-05-02 18:31:52 +0900

    Fix indenting in translator distro file

--- translator/Scannable Cite.js
+++ translator/Scannable Cite.js
@@ -19,11 +19,11 @@
 var LEGAL_TYPES = ["patent","case","statute","bill","treaty","regulation","gazette"];
 var Mem = function () {
     var isLegal = false;
-	var lst = [];
+    var lst = [];
     this.init = function (item) { lst = []; isLegal = (LEGAL_TYPES.indexOf(item.itemType)>-1)};
-	this.set = function (str, punc, slug) { if (!punc) {punc=""}; if (str) {lst.push((str + punc))} else if (!isLegal) {lst.push(slug)}};
-	this.setlaw = function (str, punc) { if (!punc) {punc=""}; if (str && isLegal) {lst.push(str + punc)}};
-	this.get = function () { return lst.join(" ") };
+    this.set = function (str, punc, slug) { if (!punc) {punc=""}; if (str) {lst.push((str + punc))} else if (!isLegal) {lst.push(slug)}};
+    this.setlaw = function (str, punc) { if (!punc) {punc=""}; if (str && isLegal) {lst.push(str + punc)}};
+    this.get = function () { return lst.join(" ") };
 }
 var mem = new Mem();
 var memdate = new Mem();
@@ -33,22 +33,22 @@ function doExport() {
     while (item = Zotero.nextItem()) {
         mem.init(item);
         Zotero.write("{ |");
-        var library_id = item.LibraryID ? item.LibraryID : 0;
-		if (item.creators.length >0){
-  			mem.set(item.creators[0].lastName,",");
-        	if (item.creators.length > 2) mem.set("et al.", ",");
-        	else if (item.creators.length == 2) mem.set("&amp; " + item.creators[1].lastName, ",");
-		}
-        else {
-			mem.set(false, ",","anon.");
+        var library_id = item.libraryID ? item.libraryID : 0;
+        if (item.creators.length >0){
+            mem.set(item.creators[0].lastName,",");
+            if (item.creators.length > 2) mem.set("et al.", ",");
+            else if (item.creators.length == 2) mem.set("&amp; " + item.creators[1].lastName, ",");
         }
-		mem.set(item.title,",","(no title)");
+        else {
+            mem.set(false, ",","anon.");
+        }
+        mem.set(item.title,",","(no title)");
         mem.setlaw(item.authority, ",");
         mem.setlaw(item.volume);
         mem.setlaw(item.reporter);
         mem.setlaw(item.pages);
         memdate.setlaw(item.court,",");
-	    var date = Zotero.Utilities.strToDate(item.date);
+        var date = Zotero.Utilities.strToDate(item.date);
         var dateS = (date.year) ? date.year : item.date;
         memdate.set(dateS,"","no date");
         Zotero.write(" " + mem.get() + " (" + memdate.get() + ") | | |");
